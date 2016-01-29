9e5e60bbcde38f67c1a0254ee5b96e2946f96860
Sean Blyden <sblyden@granger.socs.uoguelph.ca>
2016-01-29 18:36:33 -0500

    initial setup of repos

---
+++ A2_Rogue/util.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "ncurses.h"
+
+/*requirements:
+-open a file
+-parse room descriptions
+    -whole file
+    -by lines
+- initialize ncurses
+- draws level to screen
+-parse user input
+-update screen
+-clean up ncurses
+-print gold
+-print inventory
+-reload room
+-validate a user move
+-update hero gold
+-update inventory
+ */
+/*
+data rquirements:
+-gold type
+-hero
+-inventory (linked list)
+-door
+-dungeon elements
+-break these down into data-types
+ */
