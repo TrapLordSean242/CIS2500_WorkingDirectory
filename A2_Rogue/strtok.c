a57c990ef3ebc59b66becada19dd1dba59b2eac1
Sean Blyden <sblyden@mail.uoguelph.ca>
2016-02-15 04:31:08 -0500

    formatted

--- A2_Rogue/strtok.c
+++ A2_Rogue/strtok.c
@@ -14,154 +14,155 @@ int main(int argc, char* argv[])
 }
 void parseLine(char* path)
 {
-  FILE * level;
-  char buffer[150];
-  char* stuff;
-  DungeonElement* dunElement;
-  Gold* typeGold;
-  int roomx;
-  int roomy;
-  int stuffx;
-  int stuffy;
-  int counter;
-  char dump;
-  char direction;
-  char comma;
-  Rooms** room;
-  int roomCount;
-  level = fopen(path, "r");
-  roomCount = 0;
-  if(level == NULL)
+    FILE * level;
+    char buffer[150];
+    char* stuff;
+    DungeonElement* dunElement;
+    Gold* typeGold;
+    int roomx;
+    int roomy;
+    int stuffx;
+    int stuffy;
+    int counter;
+    char dump;
+    char direction;
+    char comma;
+    Rooms** room;
+    int roomCount;
+    level = fopen(path, "r");
+    roomCount = 0;
+    if(level == NULL)
     {
-      printf("Error opening file");
-      exit(0);
+        printf("Error opening file");
+        exit(0);
     }
-  room = malloc(sizeof(Rooms*));
-  while(fgets(buffer, 150, level) != NULL);
-  {
-    roomCount++;
-    room = realloc(room, sizeof(Rooms*)*roomCount);
-    printf("%s\n", buffer);
-    stuff = strtok(buffer, " ");
-    sscanf(stuff, "%d%c%d", &roomx, &dump, &roomy);
-    room[roomCount-1] = malloc(sizeof(Rooms));
-    room[roomCount-1]->height=roomy;
-    room[roomCount-1]->width=roomx;
-    room[roomCount-1]->numgDrops=0;
-    room[roomCount-1]->numElements=0;
-    printf("roomx = %d, roomy = %d\n", roomx, roomy);
+    room = malloc(sizeof(Rooms*));
+    while(fgets(buffer, 150, level) != NULL);
+    {
+        roomCount++;
+        room = realloc(room, sizeof(Rooms*)*roomCount);
+        printf("%s\n", buffer);
+        stuff = strtok(buffer, " ");
+        sscanf(stuff, "%d%c%d", &roomx, &dump, &roomy);
+        room[roomCount-1] = malloc(sizeof(Rooms));
+        room[roomCount-1]->height=roomy;
+        room[roomCount-1]->width=roomx;
+        room[roomCount-1]->numgDrops=0;
+        room[roomCount-1]->numElements=0;
+        printf("roomx = %d, roomy = %d\n", roomx, roomy);
 
-    stuff = strtok(NULL, " ");
-    while (stuff != NULL)
-      {
-	if(stuff[0] != '\0')
-	  {
-	    sscanf(stuff, "%c%d%c%d", &dump, &stuffx, &comma, &stuffy);
-	    printf("dump = %c, stuffx = %d, comma = %c, stuffy = %d\n",  dump,  stuffx,  comma,  stuffy );
-	  }
-	else
-	  {
-	    sscanf(stuff, "%c%c%d", &dump, &direction, &stuffx);
-	    printf("Dump = %c\n, Direction = %c\n, X = %d\n", dump, direction, stuffx);
-	  }
-	switch(dump){
-	case 'd':
-	  dunElement = malloc(sizeof(DungeonElement));
-          break;
-	  switch(direction)
+        stuff = strtok(NULL, " ");
+        while (stuff != NULL)
+        {
+	    if(stuff[0] != '\0')
 	    {
-	    case 'n':
-	      dunElement->y=0;
-	      dunElement->x=stuffx;
-	      break;
-	    case 's':
-	      dunElement->y=room[roomCount-1]->height;
-	      dunElement->x=stuffx;
-	      break;
-	    case 'w':
-	      dunElement->y=stuffx;
-	      dunElement->x=0;
-	      break;
-	    case 'e':
-	      dunElement->y=stuffx;
-	      dunElement->x=room[roomCount-1]->width;
-              printf("Height: %d\n", dunElement->x=room[roomCount-1]->width);
-	      break;
-	    default:
-	      printf("Something went wrong with room %d\n", roomCount);
-	      exit(1);
+	        sscanf(stuff, "%c%d%c%d", &dump, &stuffx, &comma, &stuffy);
+	        printf("dump = %c, stuffx = %d, comma = %c, stuffy = %d\n",  dump,  stuffx,  comma,  stuffy );
 	    }
-	  dunElement->rep = 'd';
-	  dunElement->type = DOOR;
-	  dunElement->next = NULL;
-	  room[roomCount-1]->numElements+=1;
-	  addDungeonElement(room[roomCount-1], dunElement);
+	    else
+	    {
+	        sscanf(stuff, "%c%c%d", &dump, &direction, &stuffx);
+	        printf("Dump = %c\n, Direction = %c\n, X = %d\n", dump, direction, stuffx);
+	    }
+	    switch(dump)
+            {
+	        case 'd':
+	        dunElement = malloc(sizeof(DungeonElement));
+                break;
+	        switch(direction)
+	        {
+	            case 'n':
+	            dunElement->y=0;
+	            dunElement->x=stuffx;
+	            break;
+	            case 's':
+	            dunElement->y=room[roomCount-1]->height;
+	            dunElement->x=stuffx;
+	            break;
+	            case 'w':
+	            dunElement->y=stuffx;
+	            dunElement->x=0;
+	            break;
+	            case 'e':
+	            dunElement->y=stuffx;
+	            dunElement->x=room[roomCount-1]->width;
+                    printf("Height: %d\n", dunElement->x=room[roomCount-1]->width);
+	            break;
+	            default:
+	            printf("Something went wrong with room %d\n", roomCount);
+	            exit(1);
+	        }
+	    dunElement->rep = 'd';
+	    dunElement->type = DOOR;
+	    dunElement->next = NULL;
+	    room[roomCount-1]->numElements+=1;
+	    addDungeonElement(room[roomCount-1], dunElement);
 	    break;
-	case 'g':
-	typeGold = malloc(sizeof(Gold));
-	typeGold->goldY=stuffx;
-	typeGold->goldX=stuffy;
-	printf("Gold X: %d\n", typeGold->goldX=stuffy);
-        break;
-        case 'M':
-        dunElement->y=stuffx;
-        dunElement->x=stuffy;
-        dunElement->rep = 'M';
-        dunElement->type = MONSTER;
-        dunElement->next = NULL;
-        room[roomCount-1]->numElements+=1;
-        addDungeonElement(room[roomCount-1], dunElement);
-        break;
-        case 'h':
-        dunElement->y=stuffx;
-        dunElement->x=stuffy;
-        dunElement->rep = 'h';
-        dunElement->type = HERO;
-        dunElement->next = NULL;
-        room[roomCount-1]->numElements+=1;
-        addDungeonElement(room[roomCount-1], dunElement);
-        break;
-        case 'p':
-        dunElement->y=stuffx;
-        dunElement->x=stuffy;
-        dunElement->rep = 'p';
-        dunElement->type = POTION;
-        dunElement->next = NULL;
-        room[roomCount-1]->numElements+=1;
-        addDungeonElement(room[roomCount-1], dunElement);
-        break;
-	default:
-	 printf("Something went wrong!\n");
-	 exit(0);
-	}
-	stuff = strtok(NULL, " ");
-        //free(dunElement);
-      }
-  }
+	    case 'g':
+	    typeGold = malloc(sizeof(Gold));
+	    typeGold->goldY=stuffx;
+	    typeGold->goldX=stuffy;
+	    printf("Gold X: %d\n", typeGold->goldX=stuffy);
+            break;
+            case 'M':
+            dunElement->y=stuffx;
+            dunElement->x=stuffy;
+            dunElement->rep = 'M';
+            dunElement->type = MONSTER;
+            dunElement->next = NULL;
+            room[roomCount-1]->numElements+=1;
+            addDungeonElement(room[roomCount-1], dunElement);
+            break;
+            case 'h':
+            dunElement->y=stuffx;
+            dunElement->x=stuffy;
+            dunElement->rep = 'h';
+            dunElement->type = HERO;
+            dunElement->next = NULL;
+            room[roomCount-1]->numElements+=1;
+            addDungeonElement(room[roomCount-1], dunElement);
+            break;
+            case 'p':
+            dunElement->y=stuffx;
+            dunElement->x=stuffy;
+            dunElement->rep = 'p';
+            dunElement->type = POTION;
+            dunElement->next = NULL;
+            room[roomCount-1]->numElements+=1;
+            addDungeonElement(room[roomCount-1], dunElement);
+            break;
+	    default:
+	    printf("Something went wrong!\n");
+	    exit(0);
+	    }
+	    stuff = strtok(NULL, " ");
+            //free(dunElement);
+        }
+    }
 }
 void addDungeonElement(Rooms* room, DungeonElement* element)
 {
-  if(room || element == NULL)
+    if(room || element == NULL)
     {
-      return;
+        return;
     }
-  DungeonElement* curElement;
-  if (room->elementHead == NULL)
+    DungeonElement* curElement;
+    if (room->elementHead == NULL)
     {
-      room->elementHead = element;
+        room->elementHead = element;
     }
-  else
+    else
     {
-      curElement = room->elementHead;
-      while (curElement != NULL)
+        curElement = room->elementHead;
+        while (curElement != NULL)
 	{
-	  if(curElement->next == NULL)
+	    if(curElement->next == NULL)
 	    {
-	      break;
+	        break;
 	    }
-	  curElement = curElement->next;
+	    curElement = curElement->next;
 	}
-      curElement->next = element;
+        curElement->next = element;
     }
 
 }
